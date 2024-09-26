dc1935e59c5d651ab3ff9c7b8107692ef28d0a64
RidhoAnfaal <ridhoanfaal0@gmail.com>
2024-09-26 09:54:48 +0700

    q1 commit

---
+++ Quiz1/Pembayaran.java
@@ -0,0 +1,18 @@
+public class Pembayaran {
+    private String idPembayaran;
+    private double jumlah;
+    private String metodePembayaran;
+    private String tanggal;
+
+    public Pembayaran(String idPembayaran, double jumlah, String metodePembayaran, String tanggal) {
+        this.idPembayaran = idPembayaran;
+        this.jumlah = jumlah;
+        this.metodePembayaran = metodePembayaran;
+        this.tanggal = tanggal;
+    }
+
+    public void tampilkanDetailPembayaran() {
+        System.out.println("ID Pembayaran: " + idPembayaran + ", Jumlah: " + jumlah + ", Metode: " + metodePembayaran
+                + ", Tanggal: " + tanggal);
+    }
+}
