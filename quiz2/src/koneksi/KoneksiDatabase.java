d2dbf7259834aeb462beda8042055a7e0c51ff7c
RidhoAnfaal <146906646+RidhoAnfaal@users.noreply.github.com>
2024-11-29 10:13:05 +0700

    q2

---
+++ quiz2/src/koneksi/KoneksiDatabase.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+/**
+ *
+ * @author MSI
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class KoneksiDatabase {
+    Connection cn;
+    public static Connection Bukakoneksi(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/sportstore", "root", "anfaal");
+            return cn;
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
---
+++ quiz2/src/quiz2/Quiz2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz2;
+
+/**
+ *
+ * @author MSI
+ */
+import java.sql.*;
+public class Quiz2 {
+    public Statement st;
+    public ResultSet rs;
+    Connection cn = koneksi.KoneksiDatabase.Bukakoneksi();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
